24228b30084dfcb2e716f917fd68d09af3990336
Maxim <86004837+potapovWebDev@users.noreply.github.com>
2023-06-23 13:06:36 +0300

    Delete dosc directory

--- dosc/main.js
+++
@@ -1,9 +0,0 @@
-/* Fancybox.show([
-    {
-      src: "https://www.youtube.com/watch?v=z2X2HaTvkl8",
-      type: "video",
-      ratio: 16 / 10,
-      width: 640,
-      height: 360,
-    },
-  ]); */
