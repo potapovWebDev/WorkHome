a617e09d72524bf7d68ed5b8df47ab3b2cee6bfe
Maxim <86004837+potapovWebDev@users.noreply.github.com>
2023-06-23 13:07:32 +0300

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,9 @@
+/* Fancybox.show([
+    {
+      src: "https://www.youtube.com/watch?v=z2X2HaTvkl8",
+      type: "video",
+      ratio: 16 / 10,
+      width: 640,
+      height: 360,
+    },
+  ]); */
