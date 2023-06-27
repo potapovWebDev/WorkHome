30208cffd635fc12baeee8000b1485496c235f6a
Maxim <86004837+potapovWebDev@users.noreply.github.com>
2023-06-27 12:11:18 +0300

    Add files via upload

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,15 @@
 $(function () {
   var mixer = mixitup('.blog__items');
- 
+  
+  $('.customers__carousel').slick({
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    draggable: false,
+    arrows: false
+  });
+
   $("#rateYo").rateYo({
- 
     "rating" : 4.5
   });
 });
