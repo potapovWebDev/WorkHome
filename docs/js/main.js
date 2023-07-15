425d110d7281d535c5eba234391d880e789f1407
Maxim <86004837+potapovWebDev@users.noreply.github.com>
2023-07-16 01:10:46 +0300

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -10,3 +10,12 @@ $('.about-slide__arrow-l').on('click', function (e){
     e.preventDefault()
     $('.about-slide__box').slick('slickNext')
   });
+
+$('.services-slide__carousel').slick({
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+});
+
+Fancybox.bind()
